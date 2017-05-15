15f59f77d01d572dad4239680f2b739a1b1df4d4
Albert Aige Cortasa <albert.aige@gft.com>
2017-05-15 14:26:43 +0200

    updated to ionic 3 and added tests dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { FormsModule }   from '@angular/forms';
 import { MyApp } from './app.component';
@@ -28,6 +30,8 @@ import { User } from '../models/user.model';
     FormPage
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     FormsModule,
     IonicModule.forRoot(MyApp)
   ],
---
+++ src/pages/login/login.spec.ts
@@ -0,0 +1,51 @@
+// import { TestBed, ComponentFixture, async, inject } from '@angular/core/testing';
+// import { IonicModule } from 'ionic-angular';
+// import { LoginPage } from './login';
+//
+// let comp: MyApp;
+// let fixture: ComponentFixture<MyApp>;
+//
+// describe('Component: Root Component', () => {
+//
+//   beforeEach(async(() => {
+//
+//     TestBed.configureTestingModule({
+//
+//       declarations: [MyApp],
+//
+//       providers: [
+//
+//       ],
+//
+//       imports: [
+//         IonicModule.forRoot(MyApp)
+//       ]
+//
+//     }).compileComponents();
+//
+//   }));
+//
+//   beforeEach(() => {
+//
+//     fixture = TestBed.createComponent(MyApp);
+//     comp    = fixture.componentInstance;
+//
+//   });
+//
+//   afterEach(() => {
+//     fixture.destroy();
+//     comp = null;
+//   });
+//
+//   it('is created', () => {
+//
+//     expect(fixture).toBeTruthy();
+//     expect(comp).toBeTruthy();
+//
+//   });
+//
+//   it('initialises with a root page of HomePage', () => {
+//     expect(comp['rootPage']).toBe(HomePage);
+//   });
+//
+// });
